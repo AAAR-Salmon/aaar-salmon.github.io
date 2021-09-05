3459f9d10509cf5cd4949c7b16cb027537db413b
AAAR-Salmon <tenzikujohn@gmail.com>
2021-09-05 22:37:57 +0900

    Add: エラーページを作成

---
+++ layouts/error.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex flex-col items-center justify-center">
+    <picture>
+      <!--suppress HtmlUnknownTarget -->
+      <source srcset="@/assets/images/error.svg" type="image/svg+xml">
+      <img src="@/assets/images/error.png" alt="エラー" class="h-60 w-60">
+    </picture>
+    <div class="text-gray-600 text-xl">
+      {{ error.statusCode }}: {{ error.message }}
+    </div>
+    <div class="text-lg">
+      <NuxtLink to="/">
+        ホーム
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    error: {
+      default: {}
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
