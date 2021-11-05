06e75031643b417cbf5ce9e80e1e73f6af88bb79
AAAR-Salmon <tenzikujohn@gmail.com>
2021-11-05 15:56:01 +0900

    Add: Google Fontを導入

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../styles/global.sass'
+import Head from 'next/head'
+import { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet"/>
+        <title>{pageProps.title ?? '記事ビューア'}</title>
+      </Head>
+      <Component {...pageProps}/>
+    </>
+  )
+}
