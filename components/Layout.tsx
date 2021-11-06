d5d62dcec8e440c0199c572680e19eab5dbbadd6
AAAR-Salmon <tenzikujohn@gmail.com>
2021-11-07 03:03:51 +0900

    Fix: titleを設定できるようにHeadをLayout componentに移動

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,23 +1,33 @@
 import React from 'react'
+import Head from 'next/head'
 import style from './Layout.module.sass'
 import Header from './Header'
 import Footer from './Footer'
 
 type Props = {
   children: React.ReactNode
+  title?: string
 }
 
-const Layout: React.VFC<Props> = ({ children, ...props }) => {
+const Layout: React.VFC<Props> = ({ children, title, ...props }) => {
   return (
-    <div className={style.layout} {...props}>
-      <Header/>
-      <div className={style.container}>
-        <div className={style.main}>
-          {children}
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet"/>
+        <title>{title ?? '記事ビューア'}</title>
+      </Head>
+      <div className={style.layout} {...props}>
+        <Header/>
+        <div className={style.container}>
+          <div className={style.main}>
+            {children}
+          </div>
         </div>
+        <Footer/>
       </div>
-      <Footer/>
-    </div>
+    </>
   )
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,9 @@
 import React from 'react'
 import '@/styles/global.sass'
-import Head from 'next/head'
 import { AppProps } from 'next/app'
 
-const App: React.VFC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet"/>
-        <title>{pageProps.title ?? '記事ビューア'}</title>
-      </Head>
-      <Component {...pageProps}/>
-    </>
-  )
+const App: React.VFC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps}/>
 }
 
 export default App
