f12ff9e9092ceb919f0b284ffe00c5e8538564de
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2021-11-13 23:55:44 +0900

    Add: テキストをそのまま読み込むだけのwebpack loaderを追加

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.txt$/,
+      use: [
+        {
+          loader: './lib/txt-loader.js'
+        }
+      ]
+    })
+    return config
+  }
 }
