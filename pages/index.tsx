d46e59c4fa144071aab50e66317e261cf1148299
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2021-11-17 20:34:52 +0900

    Add: ランディングのブランドエリア追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import Layout from '@/components/Layout'
+import Image from 'next/image'
 import Link from 'next/link'
+import style from './index.module.sass'
 
 const Home: React.VFC = () => {
-  const links = [
-    '/building-kyopro-environment/python'
-  ]
   return (
     <Layout>
-      <ul>
-        {links.map(link => (
-          <li key={link}>
-            <Link href={link}>
-              <a>{link}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <section className="spNone">
+        <div className={style.brandingArea}>
+          <div>
+            <Image
+              src="/images/salmon-logo.svg"
+              width={160}
+              height={160}
+            />
+          </div>
+          <div className={style.brandingText}>
+            <h1 className={style.headingWithLine}>AAAR Salmon</h1>
+            <p>しがない学生、終に鮭のホームページです</p>
+          </div>
+        </div>
+      </section>
     </Layout>
   )
 }
